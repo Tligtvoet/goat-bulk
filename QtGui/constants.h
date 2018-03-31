31976e59a9260d078b1edcee6b8718b44dfdffad
Tligtvoet <36016624+Tligtvoet@users.noreply.github.com>
2018-03-31 09:50:24 -0700

    UPDATED
    Fixed the need to change the location of the database file in constants.h

--- QtGui/constants.h
+++ QtGui/constants.h
@@ -4,7 +4,7 @@
 #include <QString>
 
 const int UNASSIGNED_DB_ID = -1;
-const QString DB_Path = "/Users/mitchellohair/Desktop/goat-bulk/QtGui";
+const QString DB_Path = ":/new/database/bulk.db";
 const QString LOGIN_TABLE = "LoginInfo";
 const QString MEMBER_TABLE = "Member";
 const QString INVENTORY_TABLE = "Inventory";
